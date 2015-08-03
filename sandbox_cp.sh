707a93d1507a3912f43089a908f9e4b9bb50a0df
paulkilla <paul.killer@gmail.com>
2015-08-04 09:58:20 +1000

    add argument for user

--- sandbox_cp.sh
+++ sandbox_cp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Try again with your username as first argument'
+    exit 0
+fi
+
 echo "Syncing Database from @sandbox to local..."
 drush -y @sandbox sql-dump --result-file=/tmp/cdpp.sql
 rsync $1@sandbox:/tmp/cdpp.sql /tmp/cdpp.sql
