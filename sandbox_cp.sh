a9766299d08314b4ee6630184f1814e871aa5c08
Tim Hurley <tim@timhurley.net>
2015-08-03 11:29:31 +1000

    script changes

--- sandbox_cp.sh
+++ sandbox_cp.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 echo "Syncing Database from @sandbox to local..."
 drush -y @sandbox sql-dump --result-file=/tmp/cdpp.sql
 rsync $1@sandbox:/tmp/cdpp.sql /tmp/cdpp.sql
 drush -y sql-drop
 drush -y sql-cli < /tmp/cdpp.sql
 echo "✔ Synced Database"
+
 echo "Syncing Files..."
 drush -y rsync @sandbox:%files/ @self:sites/default/files
 echo "✔ Synced Files"
 
+echo "Clearing Caches..."
+drush cc all
+echo "✔ Cached Cleared"
+
+wget -qO- http://cdpp.dev/user &> /dev/null
+echo "✔ All done."
