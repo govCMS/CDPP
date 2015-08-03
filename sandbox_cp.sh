8e715c984d9ea7a947d6afdefd7ab699dfc7da70
Tim Hurley <tim@timhurley.net>
2015-08-03 10:27:34 +1000

    better script

--- sandbox_cp.sh
+++ sandbox_cp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "Syncing Database from @sandbox to local"
-drush -y sql-sync @sandbox @self
+drush -y @sandbox sql-dump --result-file=/tmp/cdpp.sql
+rsync $1@sandbox:/tmp/cdpp.sql /tmp/cdpp.sql
+drush -y sql-drop
+drush -y sql-cli < /tmp/cdpp.sql
 echo "Synced Database"
 echo "Syncing Files"
-drush -y rsync @sandbox:%files/ @self:%files
+drush -y rsync @sandbox:%files/ @self:sites/default/files
 echo "Synced Files"
 
