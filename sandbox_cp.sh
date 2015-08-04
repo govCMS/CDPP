4376d528e4bdb48eb61d931202a362fe7258984a
Tim Hurley <tim@timhurley.net>
2015-08-04 11:00:35 +1000

    scss setup

--- sandbox_cp.sh
+++ sandbox_cp.sh
@@ -7,14 +7,21 @@ rsync $1@sandbox:/tmp/cdpp.sql /tmp/cdpp.sql
 drush -y sql-drop
 drush -y sql-cli < /tmp/cdpp.sql
 echo "✔ Synced Database"
+echo ""
 
 echo "Syncing Files..."
 drush -y rsync @sandbox:%files/ @self:sites/default/files
 echo "✔ Synced Files"
+echo ""
 
 echo "Clearing Caches..."
 drush cc all
 echo "✔ Cached Cleared"
+echo ""
 
+echo "Repopulating Cache..."
 wget -qO- http://cdpp.dev/user &> /dev/null
+wget -qO- http://cdpp.dev/ &> /dev/null
+wget -qO- http://cdpp.local/user &> /dev/null
+wget -qO- http://cdpp.local/ &> /dev/null
 echo "✔ All done."
