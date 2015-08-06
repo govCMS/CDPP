fa253fa1428ad8c3d58106d35e4556eb2469f4ae
paulkilla <paul.killer@gmail.com>
2015-08-06 16:04:07 +1000

    tables now have right class

--- cdpp/js/cdpp.js
+++ cdpp/js/cdpp.js
@@ -2,5 +2,11 @@ var Drupal = Drupal || {};
 
 (function($, Drupal){
 
+    jQuery(document).ready(function($) {
+        $('table').each(function() {
+            $(this).addClass('table');
+        });
+    });
+
 
 })(jQuery, Drupal);
