19be5c7674b7d9f046df44988b17d569bd57e3ba
Tim Hurley <tim@timhurley.net>
2015-08-07 16:28:07 +1000

    many many style changes

--- cdpp/templates/bean--image-and-text--homepage-image-text.tpl.php
+++ cdpp/templates/bean--image-and-text--homepage-image-text.tpl.php
@@ -56,14 +56,14 @@
           <?php print render($content['field_bean_image']); ?>
         <?php endif; ?>
       <?php endif; ?>
-      <p>
+      <div class="caption">
         <?php if (!empty($title)): ?>
           <h2<?php print $title_attributes; ?>><?php print $title ?></h2>
         <?php endif;?>
       <?php if (!empty($content['field_bean_text'])) : ?>
           <?php print render($content['field_bean_text']); ?>
       <?php endif; ?>
-      </p>
+    </div>
     </div>
   </div>
 </div>
