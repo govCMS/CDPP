936c4c9208b07d590cb5a63cadbadf8bc9b7089c
Tim Hurley <tim@timhurley.net>
2015-08-13 14:11:18 +1000

    updated template and css for 2nd sidebar

--- cdpp/templates/page.tpl.php
+++ cdpp/templates/page.tpl.php
@@ -176,8 +176,8 @@
       <?php print render($page['content']); ?>
     </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-12 col-md-3" role="complementary">
+    <?php if (!empty($page['sidebar_second'])):  ?>
+      <aside class="col-sm-12 col-md-3 well" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
