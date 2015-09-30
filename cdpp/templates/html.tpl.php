1bd6754d976890fe0ae0687c983c0e602cec451c
paulkilla <paul.killer@gmail.com>
2015-09-30 15:26:31 +1000

    attempt to fix font icons

--- cdpp/templates/html.tpl.php
+++ cdpp/templates/html.tpl.php
@@ -46,6 +46,7 @@
 ?><!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
